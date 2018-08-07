a6627b9b06dfaa8e67ea49e2e62e4ec27f2590bf
Martin Kan <martinkan@gmail.com>
2018-08-07 12:37:16 +0800

    First Commit

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,7 @@
 #include "Eigen-3.3/Eigen/Core"
 #include "Eigen-3.3/Eigen/QR"
 #include "json.hpp"
+#include "spline.h"
 
 using namespace std;
 
@@ -18,6 +19,10 @@ using json = nlohmann::json;
 constexpr double pi() { return M_PI; }
 double deg2rad(double x) { return x * pi() / 180; }
 double rad2deg(double x) { return x * 180 / pi(); }
+// Starts in lane 1
+int lane = 1;
+// Reference Velocity
+double ref_vel = 5; //mph
 
 // Checks if the SocketIO event has JSON data.
 // If there is data the JSON object in string format will be returned,
@@ -163,6 +168,105 @@ vector<double> getXY(double s, double d, const vector<double> &maps_s, const vec
 
 }
 
+// Check if it's safe to change lane 
+template <typename T> bool LaneChangePossible(int target_lane, double car_s, double car_speed, int prev_size, T sensor_fusion)
+{
+	bool result = true;
+	for(int i = 0; i < sensor_fusion.size(); i++)
+	{
+		float d = sensor_fusion[i][6];
+		// car is on target lane
+		if(d < (2+4*target_lane+2) && d > (2+4*target_lane-2))
+		{
+			double vx = sensor_fusion[i][3];
+			double vy = sensor_fusion[i][4];
+			double check_speed = sqrt(vx*vx+vy*vy);
+			double check_car_s = sensor_fusion[i][5];
+
+			check_car_s+=((double)prev_size*.02*check_speed); //if using previous points can project s value out
+			//check car not immediately in front
+			if((check_car_s > car_s) && ((check_car_s-car_s) < 30))
+			{
+				result = false;
+			}
+			//check car is a safe distance behind 
+			if((check_car_s < car_s) && ((car_s - check_car_s) < 30))
+			{
+				if((car_s - check_car_s) < 10 || (car_speed + 20) < check_speed)
+					result = false;
+				else if((car_s - check_car_s) < 20 && (car_speed + 10) < check_speed)
+					result = false;
+			}
+		}
+	}
+	return result;
+}
+
+// Cost function that evaluates the traffic on a lane
+template <typename T> double LaneTraffic(int target_lane, double car_s, int prev_size, T sensor_fusion)
+{
+	double result = 0.0;
+	for(int i = 0; i < sensor_fusion.size(); i++)
+	{
+		float d = sensor_fusion[i][6];
+		// car is on target lane
+		if(d < (2+4*target_lane+2) && d > (2+4*target_lane-2))
+		{
+			double vx = sensor_fusion[i][3];
+			double vy = sensor_fusion[i][4];
+			double check_speed = sqrt(vx*vx+vy*vy);
+			double check_car_s = sensor_fusion[i][5];
+
+			check_car_s+=((double)prev_size*.02*check_speed); //if using previous points can project s value out
+			//Penalty applies if traffic detected ahead of the car in a given lane
+			if((check_car_s > car_s) && ((check_car_s-car_s) < 60))
+			{
+				result += 1.0;
+			}
+		}
+	}
+	return result;
+}
+
+// Cost function that evaluates the average speed on a lane
+template <typename T> double LaneSpeed(int target_lane, double car_s, int prev_size, T sensor_fusion)
+{
+	double result = 0.0;
+	int count = 0;
+	for(int i = 0; i < sensor_fusion.size(); i++)
+	{
+		float d = sensor_fusion[i][6];
+		// car is on target lane
+		if(d < (2+4*target_lane+2) && d > (2+4*target_lane-2))
+		{
+			double vx = sensor_fusion[i][3];
+			double vy = sensor_fusion[i][4];
+			double check_speed = sqrt(vx*vx+vy*vy);
+			double check_car_s = sensor_fusion[i][5];
+
+			check_car_s+=((double)prev_size*.02*check_speed); //if using previous points can project s value out
+			//Record speed of car if criteria met
+			if((check_car_s > car_s) && ((check_car_s-car_s) < 50))
+			{
+				result += check_speed;
+				count++;
+			}
+		}
+	}
+
+	if(count > 0)
+	{
+		result = result / count; // returns average speed
+	}
+	else
+	{
+		result = 50.0; //returns speed limit
+	}
+
+	return result;
+}
+
+
 int main() {
   uWS::Hub h;
 
@@ -199,6 +303,7 @@ int main() {
   	map_waypoints_dx.push_back(d_x);
   	map_waypoints_dy.push_back(d_y);
   }
+
 
   h.onMessage([&map_waypoints_x,&map_waypoints_y,&map_waypoints_s,&map_waypoints_dx,&map_waypoints_dy](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
                      uWS::OpCode opCode) {
@@ -237,13 +342,224 @@ int main() {
           	// Sensor Fusion Data, a list of all other cars on the same side of the road.
           	auto sensor_fusion = j[1]["sensor_fusion"];
 
+          	int prev_size = previous_path_x.size();
+			
+			if(prev_size > 0)
+			{
+				car_s = end_path_s;
+			}
+
+			bool too_close = false;
+			double car_ahead_speed;
+
+			//find rev_v to use
+			for(int i = 0; i < sensor_fusion.size(); i++)
+			{
+				//car is in my lane
+				float d = sensor_fusion[i][6];
+				if(d < (2+4*lane+2) && d > (2+4*lane-2))
+				{
+					double vx = sensor_fusion[i][3];
+					double vy = sensor_fusion[i][4];
+					double check_speed = sqrt(vx*vx+vy*vy);
+					double check_car_s = sensor_fusion[i][5];
+
+					check_car_s+=((double)prev_size*.02*check_speed); //if using previous points can project s value out
+					//check s values greater than mine and s gap
+					if((check_car_s > car_s) && ((check_car_s-car_s) < 30))
+					{
+						too_close = true;
+						car_ahead_speed = check_speed;
+					}
+				}
+			}
+
+			// Below are the logic code that would apply if there is traffic ahead which causes the ego car to slow down
+			if(too_close)
+			{
+				if (ref_vel < car_ahead_speed)
+				{
+					ref_vel = car_ahead_speed; //Don't go slower than the car in front
+				}
+				else
+				{
+					ref_vel -= .224;
+				}
+
+				// If ego car is on lane 0, check if lane change to lane 1 is possible
+				if(lane == 0 && LaneChangePossible(1, car_s, car_speed, prev_size, sensor_fusion))
+				{
+					// Change lane if the average speed on either lane 1 or lane 2 is higher than the speed of the vehicle in front
+					if(LaneSpeed(1, car_s, prev_size, sensor_fusion) > car_ahead_speed || LaneSpeed(2, car_s, prev_size, sensor_fusion) > car_ahead_speed)
+						lane = 1;
+				}
+				// If ego car is on lane 1, check if lane change to either lane 0 or lane 2 is possible
+				if(lane == 1)
+				{
+					bool lane0_check = LaneChangePossible(0, car_s, car_speed, prev_size, sensor_fusion);
+					bool lane2_check = LaneChangePossible(2, car_s, car_speed, prev_size, sensor_fusion);
+					double lane0_speed = LaneSpeed(0, car_s, prev_size, sensor_fusion);
+					double lane2_speed = LaneSpeed(2, car_s, prev_size, sensor_fusion);
+
+					if(lane0_check && lane2_check)
+					{
+						// If both lane 0 and lane 2 are clear for lane change, then see which lane has a lower penalty on speed and traffic
+						if(lane0_speed > car_ahead_speed && lane0_speed > lane2_speed)
+							lane = 0;
+						else if(lane2_speed > car_ahead_speed && lane2_speed > lane0_speed)
+							lane = 2;
+						else if(LaneTraffic(0, car_s, prev_size, sensor_fusion) > LaneTraffic(2, car_s, prev_size, sensor_fusion))  // Lane speed on both lanes are the same, so check for traffic
+							lane = 2;
+						else
+							lane = 0;
+					}
+					else if(lane0_check) // Only lane 0 is clear for lane change
+					{
+
+						if(lane0_speed > car_ahead_speed)
+							lane = 0;
+					}
+					else if(lane2_check) // Only lane 2 is clear for lane change
+					{
+						if(lane2_speed > car_ahead_speed)
+							lane = 2;
+					}
+				}
+				// If ego car is on lane 2, check if lane change to lane 1 is possible
+				if(lane == 2 && LaneChangePossible(1, car_s, car_speed, prev_size, sensor_fusion))
+				{
+					// Change lane if the average speed on either lane 1 or lane 0 is higher than the speed of the vehicle in front
+					if(LaneSpeed(1, car_s, prev_size, sensor_fusion) > car_ahead_speed || LaneSpeed(0, car_s, prev_size, sensor_fusion) > car_ahead_speed)
+						lane = 1;
+				}
+			}
+			else if(ref_vel < 49.5)
+			{
+				ref_vel += .224;
+			}
+
           	json msgJson;
 
+          	// TODO: define a path made up of (x,y) points that the car will visit sequentially every .02 seconds
+
+          	// Below are the code shown in the walkthrough which uses spline to compute the ego car's trajectory
+
+          	// Create a list of widely spaced (x,y) waypoints, evenly spaced at 30m
+          	// Later we will interpolate waypoints with a spline and fill it in with more points that control speed
+          	vector<double> ptsx;
+          	vector<double> ptsy;
+
+          	// reference x, y, yaw states
+          	// either we will reference the starting point as where the car is or at the previous paths end point
+			double ref_x = car_x;
+			double ref_y = car_y;
+			double ref_yaw = deg2rad(car_yaw);
+
+			
+			//if previoius size is almost empty, use the car as starting reference
+			if(prev_size < 2)
+			{
+				//Use two points that make the path tangent to the car
+				double prev_car_x = car_x - cos(car_yaw);
+				double prev_car_y = car_y - sin(car_yaw);
+
+				ptsx.push_back(prev_car_x);
+				ptsx.push_back(car_x);
+
+				ptsy.push_back(prev_car_y);
+				ptsy.push_back(car_y);
+			}
+			// use the previous path's end point as starting reference
+			else
+			{
+				//Redefine reference state as previous path end point
+				ref_x = previous_path_x[prev_size-1];
+				ref_y = previous_path_y[prev_size-1];
+
+				double ref_x_prev = previous_path_x[prev_size-2];
+				double ref_y_prev = previous_path_y[prev_size-2];
+				ref_yaw = atan2(ref_y - ref_y_prev, ref_x - ref_x_prev);
+
+				//Use two points that make the path tangent to the previous path's end point
+				ptsx.push_back(ref_x_prev);
+				ptsx.push_back(ref_x);
+
+				ptsy.push_back(ref_y_prev);
+				ptsy.push_back(ref_y);
+
+			}
+
+			//In Frenet add evenly 30m spaced points ahead of the starting reference
+			vector<double> next_wp0 = getXY(car_s+30, (2+4*lane), map_waypoints_s, map_waypoints_x, map_waypoints_y);
+			vector<double> next_wp1 = getXY(car_s+60, (2+4*lane), map_waypoints_s, map_waypoints_x, map_waypoints_y);
+			vector<double> next_wp2 = getXY(car_s+90, (2+4*lane), map_waypoints_s, map_waypoints_x, map_waypoints_y);
+
+			ptsx.push_back(next_wp0[0]);
+			ptsx.push_back(next_wp1[0]);
+			ptsx.push_back(next_wp2[0]);
+
+			ptsy.push_back(next_wp0[1]);
+			ptsy.push_back(next_wp1[1]);
+			ptsy.push_back(next_wp2[1]);
+
+			for(int i = 0; i < ptsx.size(); i++)
+			{
+				//shift car reference angle to 0 degrees
+				double shift_x = ptsx[i] - ref_x;
+				double shift_y = ptsy[i] - ref_y;
+
+				ptsx[i] = (shift_x * cos(0-ref_yaw)-shift_y*sin(0-ref_yaw));
+				ptsy[i] = (shift_x * sin(0-ref_yaw)+shift_y*cos(0-ref_yaw));
+
+			}
+
+			// create a spline
+			tk::spline s;
+
+			// set (x, y) points to the spline
+			s.set_points(ptsx, ptsy);
+
+			// Define the actual (x,y) points we will use for the planner
           	vector<double> next_x_vals;
           	vector<double> next_y_vals;
 
+			// Start with all of the previous path points from last time
+			for(int i = 0; i < previous_path_x.size(); i++)
+			{
+				next_x_vals.push_back(previous_path_x[i]);
+				next_y_vals.push_back(previous_path_y[i]);
+			}
 
-          	// TODO: define a path made up of (x,y) points that the car will visit sequentially every .02 seconds
+			// Calculate how to break up spline points so that we travel at our desired reference velocity
+			double target_x = 30.0;
+			double target_y = s(target_x);
+			double target_dist = sqrt((target_x)*(target_x)+(target_y)*(target_y));
+
+			double x_add_on = 0;
+
+			// Fill up the rest of our path planner after filling it with previous points, here we will always output 50 points
+			for (int i = 1; i <= 50-previous_path_x.size(); i++)
+			{
+				double N = (target_dist/(.02*ref_vel/2.24));
+				double x_point = x_add_on + (target_x)/N;
+				double y_point = s(x_point);
+
+				x_add_on = x_point;
+
+				double x_ref = x_point;
+				double y_ref = y_point;
+
+				// rotate back to normal after rotating it earlier
+				x_point = (x_ref * cos(ref_yaw)-y_ref*sin(ref_yaw));
+				y_point = (x_ref * sin(ref_yaw)+y_ref*cos(ref_yaw));
+
+				x_point += ref_x;
+				y_point += ref_y;
+
+				next_x_vals.push_back(x_point);
+				next_y_vals.push_back(y_point);
+			}
+
           	msgJson["next_x"] = next_x_vals;
           	msgJson["next_y"] = next_y_vals;
 
